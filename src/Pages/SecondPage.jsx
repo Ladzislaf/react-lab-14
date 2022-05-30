cb17daf28c13ec3daefca1ecb14fbf85618dbecb
ladzislaf <ladzislaf@gmail.com>
2022-05-30 11:56:39 +0300

    broke fix

--- src/Pages/SecondPage.jsx
+++ src/Pages/SecondPage.jsx
@@ -5,7 +5,7 @@ const SecondPage = () => {
     return (
         <div style={{textAlign: 'center'}}>
             <h1>My page №2</h1>
-            <h2>Buturlya Roman (broke boy)</h2>
+            <h2>Buturlia Roman (broken broke boy)</h2>
             <h2>Current date: {new Date(Date.now()-86400000).toLocaleDateString()}</h2>
             <Catalog/>
         </div>
